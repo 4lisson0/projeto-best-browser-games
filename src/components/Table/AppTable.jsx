46a68a7b0655f2b4fb7728f43774c07118500774
Alisson Sartori <alissonsartori33@gmail.com>
2024-02-23 20:37:21 -0300

    add validation

--- src/components/Table/AppTable.jsx
+++ src/components/Table/AppTable.jsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 import {
   Table,
   Thead,
@@ -16,22 +16,22 @@ import {
   Heading,
   Alert,
   AlertIcon,
-} from '@chakra-ui/react';
+} from "@chakra-ui/react";
 
 const AppTable = () => {
-  const [user, setUser] = useState('Novato');
+  const [user, setUser] = useState("Novato");
   const [games, setGames] = useState([]);
   const [filteredGames, setFilteredGames] = useState([]);
-  const [alertMessage, setAlertMessage] = useState('');
-  const [alertType, setAlertType] = useState('success');
+  const [alertMessage, setAlertMessage] = useState("");
+  const [alertType, setAlertType] = useState("success");
   const navigateTo = useNavigate();
   const [token, setToken] = useState(null);
   const [role, setRole] = useState(null);
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
-    const token = localStorage.getItem('token');
-    const { roles } = JSON.parse(localStorage.getItem('user'));
+    const token = localStorage.getItem("token");
+    const { roles } = JSON.parse(localStorage.getItem("user"));
 
     setRole(roles[0]);
     setToken(token);
@@ -40,7 +40,7 @@ const AppTable = () => {
   const getGames = async () => {
     try {
       const response = await axios.get(
-        'https://api-best-browser-games.vercel.app/games',
+        "https://api-best-browser-games.vercel.app/games"
       );
       const games = response.data;
       setGames(games);
@@ -52,7 +52,7 @@ const AppTable = () => {
 
   const handleDelete = async (id) => {
     try {
-      const token = localStorage.getItem('token');
+      const token = localStorage.getItem("token");
 
       if (token) {
         await axios.delete(
@@ -61,44 +61,44 @@ const AppTable = () => {
             headers: {
               Authorization: `Bearer ${token}`,
             },
-          },
+          }
         );
         setGames(games.filter((game) => game._id !== id));
         setFilteredGames(filteredGames.filter((game) => game._id !== id));
-        setAlertType('success');
-        setAlertMessage('Game excluído com sucesso');
+        setAlertType("success");
+        setAlertMessage("Game excluído com sucesso");
       } else {
-        setAlertType('error');
+        setAlertType("error");
         setAlertMessage(
-          'Não foi possível excluir o game. Token não encontrado.',
+          "Não foi possível excluir o game. Token não encontrado."
         );
       }
     } catch (error) {
-      console.error('Erro ao excluir game:', error);
-      setAlertType('error');
-      setAlertMessage('Erro ao excluir game');
+      console.error("Erro ao excluir game:", error);
+      setAlertType("error");
+      setAlertMessage("Erro ao excluir game");
     }
   };
 
   useEffect(() => {
-    const user = localStorage.getItem('user');
+    const user = localStorage.getItem("user");
     if (user) {
       setUser(JSON.parse(user).name);
     } else {
-      setUser('Novato');
+      setUser("Novato");
     }
 
     getGames();
   }, []);
 
   useEffect(() => {
-    if (searchTerm === '') {
+    if (searchTerm === "") {
       setFilteredGames(games);
     } else {
       const filtered = games.filter(
         (game) =>
           game.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-          game.category.name.toLowerCase().includes(searchTerm.toLowerCase()),
+          game.category.name.toLowerCase().includes(searchTerm.toLowerCase())
       );
       setFilteredGames(filtered);
     }
@@ -109,7 +109,7 @@ const AppTable = () => {
   };
 
   const handleClearSearch = () => {
-    setSearchTerm('');
+    setSearchTerm("");
   };
 
   return (
@@ -150,19 +150,19 @@ const AppTable = () => {
             ) : (
               filteredGames.map((game) => (
                 <Tr key={game._id}>
-                  <Td fontSize={15}>{game.name}</Td>
-                  <Td fontSize={15}>{game.category.name}</Td>
+                  <Td fontSize={15}>{game.name || "N/A"}</Td>
+                  <Td fontSize={15}>{game.category?.name || "N/A"}</Td>
                   <Td fontSize={15} isNumeric>
-                    {game.score}
+                    {game.score || "N/A"}
                   </Td>
                   <Td fontSize={15}>
                     <Button background="#bdeb07">
-                      <a href={game.url}>Learn More</a>
+                      <a href={game.url || "#"}>Learn More</a>
                     </Button>
                   </Td>
                   <Td>
                     <Button background="#bdeb07">
-                      <a href={game.videoURL}>Video</a>
+                      <a href={game.videoURL || "#"}>Video</a>
                     </Button>
                   </Td>
                   <Td>
@@ -173,15 +173,17 @@ const AppTable = () => {
                       More Details
                     </Button>
                   </Td>
-                  <Td fontSize={15}>{game.description}</Td>
+                  <Td fontSize={15}>{game.description || "N/A"}</Td>
                   <Td>
-                    <img
-                      src={game.imageURL}
-                      style={{ maxWidth: '100px' }}
-                      alt="Game Image"
-                    />
+                    {game.imageURL && (
+                      <img
+                        src={game.imageURL}
+                        style={{ maxWidth: "100px" }}
+                        alt="Game Image"
+                      />
+                    )}
                   </Td>
-                  {token && role === 'admin' && (
+                  {token && role === "admin" && (
                     <Td>
                       <Button
                         background="#bdeb07"
